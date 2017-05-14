e93e0de6c9ced16fc538c600bd1922cdb9c6faa2
tummychow <tummychow@users.noreply.github.com>
2017-05-14 08:22:23 -0400

    add use-package

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
 (setq custom-file (concat user-emacs-directory "custom.el")
       custom-buffer-done-kill t)
 (load custom-file t)
+
+(require 'package)
+;; the default archives list uses a non-TLS address for GNU ELPA
+;; https://git.savannah.gnu.org/cgit/emacs.git/commit/?id=048133d4886d2e7fa547879478127edc9a9243f6
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+(eval-when-compile (require 'use-package))
